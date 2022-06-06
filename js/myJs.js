19076d8f36141596ae19a5dfd839e42057d18529
silasprod <106929483+silasprod@users.noreply.github.com>
2022-06-06 11:21:17 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/kayn203/"
+            window.location = "https://www.facebook.com/kayn203/";
           },
         });
       }
